6d5fef9185567f7d8418634c996c2119c39d06cc
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 02:37:12 +0530

    Shows no.of time heads and tails occure.

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -2,28 +2,37 @@
 #This problem displays the winner Heads or Tails
 echo "Wecome to Flip coin Simulation problem. "
 #Ask user to how much time they want to flip coin
-read -p "How many time you want to flipping coin? " No_Of_Times
+#read -p "How many time you want to flipping coin? " No_Of_Times
 #variables
 Head=1;
 Tail=0;
 Count_Of_Heads=0;
 Count_Of_Tails=0;
-while (( $No_Of_Times ))
+while True
 do
 	Flip_Coin=$((RANDOM%2))
 	#Random generate 0,1 values.
 	if [[ $Flip_Coin -eq $Head ]]
 	then
 		#if Flip_Coin value 1.
-		echo "It's Heads"
+		#echo "It's Heads"
 		((Count_Of_Heads++))
 	else
 		#If Flip_Coin_value 0.
-		echo "It's Tails"
+		#echo "It's Tails"
 		((Count_Of_Tails++))
 	fi
-	((No_Of_Times--))
+	#((No_Of_Times--))
+	if(( $Count_Of_Heads==21 )) 
+	then
+		echo "head won :"$(( $Count_Of_Heads - $Count_Of_Tails ))
+		break
+	elif(( $Count_Of_Tails == 21 ))
+	then
+		echo "Tails won :"$((  $Count_Of_Tails - $Count_Of_Heads ))
+		break
+	fi
 done
 
-echo "Number's of heads comes are: " $Count_Of_Heads
-echo "Number's Of tails comes are: " $Count_Of_Tails
+#echo "Number's of heads comes are: " $Count_Of_Heads
+#echo "Number's Of tails comes are: " $Count_Of_Tails
