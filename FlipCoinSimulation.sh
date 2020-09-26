86f115ef198730712aced5fd48a60c650b838eec
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 02:16:25 +0530

    Flip coin to display head or tail

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 #This problem displays the winner Heads or Tails
 echo "Wecome to Flip coin Simulation problem. "
+#variables
+Head=1;
+Tail=0;
+Flip_Coin=$((RANDOM%2))
+#Random generate 0,1 values.
+if [[ $Flip_Coin -eq $Head ]]
+then
+	#if Flip_Coin value 1.
+	echo "It's Heads"
+else
+	#If Flip_Coin_value 0.
+	echo "It's Tails"
+fi
